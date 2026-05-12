28ad449993363ea319f140980f721f5103ebd826
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-12 15:35:41 -0400

    falta exceptionhandler

---
+++ src/main/java/cl/bibliotecaam/asistencia/asistencia/config/DataInitializer.java
@@ -0,0 +1,38 @@
+package cl.bibliotecaam.asistencia.asistencia.config;
+
+import cl.bibliotecaam.asistencia.asistencia.model.Asistencia;
+import cl.bibliotecaam.asistencia.asistencia.repository.AsistenciaRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final AsistenciaRepository asistenciaRepository;
+
+    @Override
+    public void run(String... args){
+        if (asistenciaRepository.count()>0){
+            log.info(">>> Data Initializer: La BD ya tiene datos, se omite la carga inicial.");
+            return;
+        }
+
+        log.info(">>> Data Initializer: BD vacia detectada. Insertando datos de prueba.");
+
+        Asistencia asistencia1 = asistenciaRepository.save(new Asistencia(
+                null, 001L, 001L
+        ));
+
+        Asistencia asistencia2 = asistenciaRepository.save(new Asistencia(
+                null, 002L, 055L
+        ));
+
+        Asistencia asistencia3 = asistenciaRepository.save(new Asistencia(
+                null, 999L, 888L
+        ));
+    }
+}
