a8482428f3401851a18b175cf823a91d70b199c8
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-12 15:08:22 -0400

    service listo

--- src/main/java/cl/bibliotecaam/asistencia/asistencia/service/AsistenciaService.java
+++ src/main/java/cl/bibliotecaam/asistencia/asistencia/service/AsistenciaService.java
@@ -1,11 +1,13 @@
 package cl.bibliotecaam.asistencia.asistencia.service;
 
+import cl.bibliotecaam.asistencia.asistencia.dto.AsistenciaRequestDTO;
 import cl.bibliotecaam.asistencia.asistencia.dto.AsistenciaResponseDTO;
 import cl.bibliotecaam.asistencia.asistencia.model.Asistencia;
 import cl.bibliotecaam.asistencia.asistencia.repository.AsistenciaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -46,6 +48,23 @@ public class AsistenciaService {
                 .stream()
                 .map(this::mapToDTO)
                 .collect(Collectors.toList());
+    }
+
+    public Asistencia guardar(Asistencia asistencia){
+        return asistenciaRepository.save(asistencia);
+    }
+
+    public void eliminarPorId(Long id){
+        asistenciaRepository.deleteById(id);
+    }
+
+    public Optional<AsistenciaResponseDTO> actualizar(Long id, AsistenciaRequestDTO doto){
+        return asistenciaRepository.findById(id).map(existente -> {
+            existente.setIdUsuario(doto.getIdUsuario());
+            existente.setIdTaller(doto.getIdTaller());
+
+            return mapToDTO(existente);
+        });
     }
 
 }
