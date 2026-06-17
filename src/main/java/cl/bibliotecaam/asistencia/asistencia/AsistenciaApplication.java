5a900e3005cdfc29083198fd041eb4998fa49764
VI-Alumno <VI-Alumno@duoc.acad>
2026-06-17 08:51:31 -0400

    Olvide el DiscoveryClient, deberia funcionar ahora :D

--- src/main/java/cl/bibliotecaam/asistencia/asistencia/AsistenciaApplication.java
+++ src/main/java/cl/bibliotecaam/asistencia/asistencia/AsistenciaApplication.java
@@ -2,8 +2,10 @@ package cl.bibliotecaam.asistencia.asistencia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AsistenciaApplication {
 
 	public static void main(String[] args) {
